f67432ac0545bdaed00932abf214af88579f5c81
Subodh Kumar <subodhkumarjc@gmail.com>
2020-04-13 11:01:57 +0530

    Initial Changes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { RouteTwoComponent } from './route-two/route-two.component';
+import { RouteOneComponent } from './route-one/route-one.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: "full",
+    component: HomeComponent
+  },
+  {
+    path: 'route1',
+    component: RouteOneComponent
+  },
+  {
+    path: 'route2',
+    component: RouteTwoComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
